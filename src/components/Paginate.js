01e789042e2c001b4d558d0b7fa8b01d06c2859c
Swetabja Hazra <swetabjahazra@Swetabjas-Awesome-Mac.local>
2019-12-08 06:10:51 +0530

    Implemented Pagination

---
+++ src/components/Paginate.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Pagination, PaginationItem, PaginationLink } from 'reactstrap';
+import PropTypes from 'prop-types';
+
+
+const Paginate = props => {
+
+  const { itemsPerPage, pagesCount, currentPage, setCurrentPage } = props;
+
+  const handleClick = (e, index) => {
+    e.preventDefault();
+    setCurrentPage(index);
+  };
+
+  return (
+    <div className="pagination-wrapper">
+
+      <Pagination size="lg" aria-label="Page navigation example">
+        <PaginationItem disabled={currentPage <= 0}>
+          <PaginationLink onClick={e => handleClick(e, 0)} first href="#" />
+        </PaginationItem>
+        <PaginationItem disabled={currentPage <= 0}>
+          <PaginationLink
+            onClick={e => handleClick(e, currentPage - 1)}
+            previous
+            href="#"
+          />
+        </PaginationItem>
+
+        {[...Array(pagesCount)].map((page, i) => (
+          <PaginationItem active={i === currentPage} key={i}>
+            <PaginationLink onClick={e => handleClick(e, i)} href="#">
+              {i + 1}
+            </PaginationLink>
+          </PaginationItem>
+        ))}
+
+        <PaginationItem disabled={currentPage >= pagesCount - 1}>
+          <PaginationLink
+            onClick={e => handleClick(e, currentPage + 1)}
+            next
+            href="#"
+          />
+        </PaginationItem>
+        <PaginationItem disabled={currentPage >= pagesCount - 1}>
+          <PaginationLink
+            onClick={e => handleClick(e, pagesCount - 1)}
+            last
+            href="#"
+          />
+        </PaginationItem>
+      </Pagination>
+
+    </div>
+  );
+};
+
+Paginate.propTypes = {
+  itemsPerPage: PropTypes.number.isRequired,
+  pagesCount: PropTypes.number.isRequired,
+  currentPage: PropTypes.number.isRequired,
+  setCurrentPage: PropTypes.func.isRequired
+};
+
+export default Paginate;
--- src/pages/Home/GlobalFeed.js
+++ src/pages/Home/GlobalFeed.js
@@ -1,14 +1,26 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import ArticleItem from '../../components/ArticleItem';
+import Paginate from '../../components/Paginate';
+import { makeStyles } from '@material-ui/styles';
 
+const useStyles = makeStyles(() => ({
+  paginateBox: {
+    width: '100%',
+    alignContent: 'center',
+    margin: 15
+  }
+}));
 
 const GlobalFeed = () => {
-
-  const [screams, setScreams] = useState(null);
   
-  const fetchScreams = () => {
-    axios.get('/screams')
+  const classes = useStyles();
+  const [screams, setScreams] = useState(null);
+  const [currentPage, setCurrentPage] = useState(0);
+  const itemsPerPage = 4;
+  
+  const fetchScreams = async () => {
+    await axios.get('/screams')
       .then(res => {
         console.log(res.data);
         setScreams(res.data)
@@ -23,15 +35,27 @@ const GlobalFeed = () => {
   }, []);
 
   const screamsList = screams ? (
-    screams.map(scream => <ArticleItem key={scream.screamId} scream={scream} ></ArticleItem>)
+    screams.map(scream => <ArticleItem key={scream.screamId} scream={scream}></ArticleItem>)
   ) : <p>Loading...</p>;
+
+  let pagesCount = screamsList.length > 0 ? Math.ceil(screamsList.length / itemsPerPage) : 6;
+
+  const paginatedList = screamsList.length > 0 ? 
+    screamsList.slice(currentPage * itemsPerPage, (currentPage + 1) * itemsPerPage) : screamsList;
 
   return (
     <div>
-      {screamsList}
+      {paginatedList}
+      <div className={classes.paginateBox}>
+        <Paginate
+          itemsPerPage={itemsPerPage}
+          pagesCount={pagesCount}
+          currentPage={currentPage}
+          setCurrentPage={setCurrentPage}
+        />
+      </div>
     </div>
   );
-
 };
 
 
